25a69c950261b31d032d8f5f10e5d245a4d30b7c
IanH <ian_web1@hubbertz.de>
2017-02-27 01:11:34 +0100

    Support for animation during transition

--- src/OLEDStatusIndicator.h
+++ src/OLEDStatusIndicator.h
@@ -18,34 +18,17 @@ class OLEDStatusIndicator : public OLEDOverlay {
 public:
 	//OLEDStatusIndicator(SSD1306& display); // TODO: Allow to set reference to Display (or abstraction layer)
 	OLEDStatusIndicator();
-	void Event(HomieEvent event);
+	void Event(const HomieEvent& event);
 
 	virtual void drawOverlay(OLEDDisplay& display,  OLEDDisplayUiState& state, uint8_t idx);
 
 
 private:
-
-
 	bool mqtt;
 	bool wifi;
 	bool cfgmode;
 	int wifi_quality;
-
-	//SSD1306Wire display;
-	//OLEDDisplayUi ui;
-
-	bool updateNecessary;
 	String last_status;
-	uint16_t cycle_time;
-
-	Bounce but_fwd;
-	Bounce but_bwd;
-	Bounce but_clk;
-
-
-
-
-
 };
 
 #endif /* SRC_OLEDSTATUSINDICATOR_H_ */
