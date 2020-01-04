bf9aab352eebf3fc53154c2644fe90c424cc8a56
Ian Hubbertz <ian_web1@hubbertz.de>
2020-01-04 12:49:55 +0100

    Update InputPinArray_Example.cpp

--- examples/InputPinArray_Example.cpp
+++ examples/InputPinArray_Example.cpp
@@ -10,11 +10,6 @@
 
 #define FW_VERSION "1.0.0"
 
-/* Magic sequence for Autodetectable Binary Upload */
-const char *__FLAGGED_FW_NAME = "\xbf\x84\xe4\x13\x54" FW_NAME "\x93\x44\x6b\xa7\x75";
-const char *__FLAGGED_FW_VERSION = "\x6a\x3f\x3e\x0e\xe1" FW_VERSION "\xb0\x30\x48\xd4\x1a";
-/* End of magic sequence for Autodetectable Binary Upload */
-
 RGBWNode white1("LED_W1", RGBWNode::NOPIN, RGBWNode::NOPIN, RGBWNode::NOPIN, 0);
 RGBWNode white2("LED_W2", RGBWNode::NOPIN, RGBWNode::NOPIN, RGBWNode::NOPIN, 2);
 
